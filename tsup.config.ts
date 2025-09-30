a28663e6317335d29a8320e05a030e6994b99e75
cakezero <sareahj.com@gmail.com>
2025-09-30 13:55:21 +0100

    sdk update

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["src/index.ts"], // entry file
+	entry: ["./src/index.ts"], // entry file
 	format: ["cjs", "esm"], // build both
 	dts: true, // generate .d.ts
 	clean: true, // clean dist before build
-	sourcemap: true,
-	outDir: "dist",
+	shims: true,
+	skipNodeModulesBundle: true,
 });
